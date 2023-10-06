da911ef856dec003cfe75c4abb6c28130d4c022f
myne145 <antoni.kucz@gmail.com>
2023-10-06 15:08:14 +0200

    end of the lesson

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 import static Main.Algorithms.*;
@@ -46,7 +47,28 @@ public class Main {
         System.out.println("Najniższa suma: " + lowestSum + "\tLiczba: " + content.get(lowestSumIndex));
     }
 
+    private static void zad4_c() throws IOException {
+        ArrayList<Integer> content = readFileArr(new File("cyfry.txt"));
+        ArrayList<Integer> result = new ArrayList<>();
+        int tempIndex = 0;
+        for(Integer i : content) {
+           String s = String.valueOf(i);
+           int tempNumber = 0;
+           boolean hasSequence = true;
+           for(char c : s.toCharArray()) {
+               if(Integer.parseInt(String.valueOf(c)) < tempNumber) {
+                   hasSequence = false;
+               }
+           }
+           if(hasSequence)
+               result.add(i);
+        }
+
+        for(Integer i : result)
+            System.out.println(i);
+    }
+
     public static void main(String[] args) throws IOException {
-        zad4_b();
+        zad4_c();
     }
 }
