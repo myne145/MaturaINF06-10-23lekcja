5c29bef12f95bc09a6a89cc051a5f7c3a107db92
myne145 <antoni.kucz@gmail.com>
2023-10-06 14:46:48 +0200

    b easy as fuck as well

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,7 +15,38 @@ public class Main {
         }
         System.out.println(evenNumbers);
     }
+
+    private static void zad4_b() throws IOException {
+        ArrayList<Integer> content = readFileArr(new File("cyfry.txt"));
+        int highestSum = 0;
+        int highestSumIndex = 0;
+
+        int lowestSum = Integer.MAX_VALUE;
+        int lowestSumIndex = 0;
+
+        int index = 0;
+        for(Integer i : content) {
+            String s = String.valueOf(i);
+            int sum = 0;
+            for(char c : s.toCharArray()) {
+                sum += Integer.parseInt(String.valueOf(c));
+            }
+            if(sum > highestSum) {
+                highestSum = sum;
+                highestSumIndex = index;
+            }
+            if(sum < lowestSum) {
+                lowestSum = sum;
+                lowestSumIndex = index;
+            }
+
+            index++;
+        }
+        System.out.println("Najwyższa suma: " + highestSum + "\tLiczba: " + content.get(highestSumIndex));
+        System.out.println("Najniższa suma: " + lowestSum + "\tLiczba: " + content.get(lowestSumIndex));
+    }
+
     public static void main(String[] args) throws IOException {
-        zad4_a();
+        zad4_b();
     }
 }
